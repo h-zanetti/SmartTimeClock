4468161c181544ee5b20de729a9ae60f0aa38798
h-zanetti <henriaguiar99@gmail.com>
2018-12-19 16:57:53 -0600

    Update

--- smartClock.py
+++ smartClock.py
@@ -58,6 +58,38 @@ def getInfo(ID):
     else:
         return None
 
+def dateTime():
+    dt = str(datetime.datetime.now())
+    date = dt[0:10]
+    time = dt[11:16]
+    dateTime = [date, time]
+    return dateTime
+
+def getHours(ID):
+    info = getInfo(ID)
+    if info != None:
+        name = info[1]
+        dt = dateTime()
+        total = 0
+        query = "SELECT * FROM {0} WHERE date = '{1}'"
+        mycursor.execute(query.format(name, dt[0]))
+        rows = mycursor.fetchall()
+        for row in rows:
+            if row[2] != None:
+                Hrs = int(row[2][0:2]) - int(row[1][0:2])
+                Min = (60 - int(row[1][3:5]) + int(row[2][3:5]))/60
+                if Hrs == 0:
+                    Min = (int(row[2][3:5]) - int(row[1][3:5]))/60
+                    total = Min
+                elif Hrs == 1:
+                    total = Min
+                else:
+                    total = Hrs + Min
+        query = "UPDATE {0} SET hours = {1} WHERE hours is NULL"
+        mycursor.execute(query.format(name.lower(), round(total, 2)))
+    else:
+        print("invalid identification, make sure you are registered on the system and try again.")
+
 def clock_in(ID):
     info = getInfo(ID)
     if info != None:
@@ -65,11 +97,9 @@ def clock_in(ID):
         print("Welcome back, {0}.".format(info[2])) # Greets employee by the first name
         confirmation = input("Are you clocking in? [Y/N] ")
         if confirmation.lower() == 'y':
-            dateTime = str(datetime.datetime.now())
-            date = dateTime[0:10]
-            time = dateTime[11:16]
+            dt = dateTime()
             query = "INSERT INTO {0} (date, clock_in) VALUES ('{1}', '{2}')"
-            mycursor.execute(query.format(name.lower(), date, time))
+            mycursor.execute(query.format(name.lower(), dt[0], dt[1]))
     else:
         print("Invalid identification, make sure you are registered on the system and try again.")
 
@@ -79,20 +109,22 @@ def clock_out(ID):
     info = getInfo(ID)
     if info != None:
         name = info[1]
-        date = str(datetime.datetime.now())[0:10]
+        dt = dateTime()
         query = "SELECT * FROM {0} WHERE date = '{1}'"
-        mycursor.execute(query.format(name, date))
-        row = mycursor.fetchone()
-        if row != None and row[2] == None:
-            confirmation = input("{0}, are you clocking out? [Y/N] ".format(info[2]))
-            if confirmation.lower() == 'y':
-                dateTime = str(datetime.datetime.now())
-                time = dateTime[11:16]
-                query = "UPDATE {0} SET clock_out = '{1}' WHERE date = '{2}'"
-                mycursor.execute(query.format(name.lower(), time, date))
-        else:
-            clock_in(ID)
+        mycursor.execute(query.format(name.lower(), dt[0]))
+        rows = mycursor.fetchall()
+        for row in rows:
+            if row != None and row[2] == None:
+                confirmation = input("{0}, are you clocking out? [Y/N] ".format(info[2]))
+                if confirmation.lower() == 'y':
+                    dt = dateTime()
+                    query = "UPDATE {0} SET clock_out = '{1}' WHERE clock_out is NULL"
+                    mycursor.execute(query.format(name.lower(), dt[1]))
+        getHours(ID)
+        print("You are off the clock.")
     else:
         print("Invalid identification, make sure you are registered on the system and try again.")
 
     mydb.commit()
+
+clock_in(673591)
