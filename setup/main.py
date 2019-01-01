a76530f01e931ea41af021278c54a39d923ee0bb
h-zanetti <henriaguiar99@gmail.com>
2018-12-31 19:57:33 -0600

    setup 1.5

--- setup/main.py
+++ setup/main.py
@@ -1,4 +1,18 @@
-import setupDB
-from functions import addEmployee
+from setupDB import mydb
+from functions import addEmployee, mycursor, empData
 
-addEmployee()
+colum = []
+for d in empData:
+    colum.append(d)
+query = "CREATE TABLE employees ({1}{2})"
+mycursor.execute(query.format(' VARCHAR(255), '.join(colum), ' VARCHAR(255)'))
+mydb.commit()
+
+while True:
+    cmd = input()
+    if cmd == 'add':
+        addEmployee()
+    elif cmd == 'quit':
+        break
+    else:
+        print("Invalid command...")
